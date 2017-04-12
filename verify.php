81d1398c6d61c3dd36dd881e7b2da6ada63b412c
jordanrosal59 <jar59@njit.edu>
2017-04-12 14:35:24 -0400

    updated verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,3 @@
-#!/usr/bin/php
 <html>
 
 <head>
@@ -7,25 +6,25 @@
 </head>
 
 <?php
-$dbserver = "localhost"; //might be 127.0.0.1
-$username = "root";
-$password = "Jrosal1219";
-$dbname = "login";
+$dbserver = "sql2.njit.edu"; //might be 127.0.0.1 depending on backend or who has db
+$username = "jar59"; //again depends on what user uses
+$password = "popish75"; //change depending on user
+$dbname = "jar59"; //change depending on user
 
 /*
 send in user input as parameter, check against usernames in Db
 returns true if one is found, return false if not
 */
 
-function usernameVerify($unameIn)
+function usernameVerify($conn,$unameIn)
 {
  
-    $query = "SELECT username FROM users";
-    $result = mysql_query($query);
-	 
-    while($row =  mysql_fetch_assoc($result)){  //compare input with row stuff, no echo
+    $query = "SELECT username FROM Users";
+    $result = mysqli_query($conn,$query);
+
+    while($row = mysqli_fetch_array($result)){  //compare input with row stuff, no echo
 	      foreach ($row as $element) {
-	          if(strcmp($unameIn,$element) == 0) {
+		  if(strcmp((string)$unameIn,(string)$element) == 0) {
                     return true;   
                   }
 	      }
@@ -35,71 +34,51 @@ function usernameVerify($unameIn)
 }
 
 /*
-From Db take the salt and append to user input
-compare user-input+salt to password+salt(hashed) saved on server in Db
+From Db take the password from matching username
+compare user-input to password saved on server in Db
+Will also need to check salt as well as hashing, save salt in Db as well
+append salt to the input password then hash the inputpass+salt combo
+salt+password hash will be saved in Db once user registered.
 */
 function passwordVerify($unameIn, $passIn, $dbConn)
 {   
-
     $intoSQL = mysqli_real_escape_string($dbConn, $unameIn);    
 
-    $query = "SELECT salt FROM users WHERE username='".$intoSQL."'";
-    $result = mysql_query($query); //Should be salt of appropriate user to check pass
-    
-    $compPass=$passIn . $result; //input-password + salt
+    $query = "SELECT password FROM Users WHERE username='".$intoSQL."'";
+    $result = mysqli_query($dbConn,$query); 
+    $userPass = mysqli_fetch_array($result);
 
-    $query = "SELECT password FROM users WHERE username='".$intoSQL."'";
-    $userPass = mysql_query($query); 
-
-    if(strcmp($userPass,SHA1($compPass)) == 0) {
-         return true;   
+    if(strcmp((string)$userPass[0],(string)$passIn) == 0) {
+	 return true;   
     }
     
    return false;
          
 }
 
-/*
 
-function addUser() //take in username, password. will get salt (hashed? SHA1()) here from
-                   //mcrypt_create_iv? when saving to Db save username, salt+pass, salt
-{  //basically when asked to create check database if not exist then do(mysql code?)(transaction?)
-
-//prepared statements look something like this, check book as well.
-
-$query = $mysqli->prepare("INSERT INTO `table` (`col1`, `col2`) VALUES (?, ?)");
-$col1 = 100;
-$col2 = 14;
-$query->bind_param('ii', $col1, $col2);
-$query->execute();
-$query->close();
-}
-
-*/
-
-$conn = mysql_connect($dbserver,$username,$password,$dbname);
+$conn = mysqli_connect($dbserver,$username,$password,$dbname);
      if (!$conn) {
-          die("Connection failed: " . mysql_error());
+          die("Connection failed: " . mysqli_error());
      }
-mysql_select_db($dbname);
+mysqli_select_db($dbname);
 
-$usernmIn=$_GET["tableChoice"];
-$userpassIn=$_GET["..."];
 
+$usernmIn=$_POST["studUsernm"]; //post or get
+$userpassIn=$_POST["studPass"]; //post or get
 /* else-if ladder starting with username, if not exist alert(),
    moving onto password check, if not verified alert()
 */
-
-if(usernameVerify($usernmIn)) {
+if(usernameVerify($conn,$usernmIn)) {
   if(passwordVerify($usernmIn,$userpassIn,$conn)) {
-    alert("You are successfully logged in");
+    //wherever we need to be directed to once verified
   }
   else {
-    alert("Invalid Username or Password");
+    echo "Invalid Username or Password because password";
   }
 }
 else {
-  alert("Invalid Username or Password");
+  echo "Invalid Username or Password because username";
 }
 
 ?>
